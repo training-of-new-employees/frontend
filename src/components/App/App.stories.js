6bdac596cd48cddac90a4185f0d87d92b28577ca
RTemiy <RTemiyGo@yandex.ru>
2023-11-20 17:12:11 +0300

    fix: storybook router error

--- src/components/App/App.stories.js
+++ src/components/App/App.stories.js
@@ -1,8 +1,11 @@
+import { withRouter } from 'storybook-addon-react-router-v6';
 import App from './App';
 
 export default {
 	title: 'Workflow/App',
 	component: App,
+	decorators: [withRouter],
+	render: () => <App />,
 	tags: ['autodocs'],
 };
 
