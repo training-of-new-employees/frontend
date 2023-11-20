e09ff9a984894c3b37373af594a8163d5aca27be
Klimanova Julia <sempppai124@yandex.ru>
2023-11-20 17:25:33 +0300

    fix: fix error paths constant

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import paths from '../../utils/constants';
+import { paths } from '../../utils/constants';
 
 import appStyles from './App.module.scss';
 
