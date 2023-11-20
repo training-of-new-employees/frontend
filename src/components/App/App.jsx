d6f65c0a876aeaa64a9a5c4f76f794acacc2dbda
Klimanova Julia <sempppai124@yandex.ru>
2023-11-20 17:33:25 +0300

    fix: routes in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,24 +6,24 @@ import appStyles from './App.module.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
-import Login from '../../pages/login/Login';
-import Register from '../../pages/register/Register';
-import ForgotPassword from '../../pages/forgotPassword/ForgotPassword';
-import ResetPassword from '../../pages/resetPassword/ResetPassword';
-import Profile from '../../pages/profile/Profile';
-import NotFound from '../../pages/notFound/NotFound';
+import Login from '../../pages/Login/Login';
+import Register from '../../pages/Register/Register';
+import ForgotPassword from '../../pages/ForgotPassword/ForgotPassword';
+import ResetPassword from '../../pages/ResetPassword/ResetPassword';
+import Profile from '../../pages/Profile/Profile';
+import NotFound from '../../pages/NotFound/NotFound';
 
 const App = () => (
 	<div className={appStyles.app}>
 		<Header />
 		<Routes>
-			<Route path={paths.main} element={Main} />
-			<Route path="*" element={Login} />
-			<Route path="*" element={Register} />
-			<Route path="*" element={ForgotPassword} />
-			<Route path="*" element={ResetPassword} />
-			<Route path="*" element={Profile} />
-			<Route path="*" element={NotFound} />
+			<Route path={paths.main} element={<Main />} />
+			<Route path="*" element={<Login />} />
+			<Route path="*" element={<Register />} />
+			<Route path="*" element={<ForgotPassword />} />
+			<Route path="*" element={<ResetPassword />} />
+			<Route path="*" element={<Profile />} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 		<Footer />
 	</div>
