fbddc11344989d925eb47fa0e701381309426d80
Klimanova Julia <sempppai124@yandex.ru>
2023-11-20 17:40:05 +0300

    fix: add pages directory

---
+++ src/pages/ResetPassword/ResetPassword.stories.js
@@ -0,0 +1,11 @@
+import ResetPassword from './ResetPassword';
+
+export default {
+	title: 'Workflow/ResetPassword',
+	component: ResetPassword,
+	tags: ['autodocs'],
+};
+
+export const Primary = {
+	args: {},
+};
