2faa285e8e3b4b505d24b67eaf29042e129bf9c9
SergNoyt <you@example.com>
2023-12-14 02:29:55 +0400

    create file NoCureses

---
+++ src/components/NoCourses/NoCourses.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+import style from './NoCourses.module.scss';
+
+export default function NoCourses() {
+  return <section>q</section>;
+}
+
+NoCourses.propTypes = {};
+
+NoCourses.defaultProps = {};
