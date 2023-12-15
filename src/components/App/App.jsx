7e4f51606a5bc53e623dac78893e1dd74e7ccd15
Elizaveta Parada <parada_ev@mail.ru>
2023-12-15 03:23:56 +0100

    add Profile User

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,7 +18,7 @@ import NotFound from '../../pages/NotFound/NotFound';
 export default function App() {
   return (
     <div className={appStyles.app}>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path={paths.main} element={<Main />} />
         <Route path="*" element={<Login />} />
