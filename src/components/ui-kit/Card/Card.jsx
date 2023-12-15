d4f531fa9dd8c6167cd98996c9737eb723f99ef4
Elizaveta Parada <parada_ev@mail.ru>
2023-12-15 02:41:49 +0100

    add status color card

--- src/components/ui-kit/Card/Card.jsx
+++ src/components/ui-kit/Card/Card.jsx
@@ -6,30 +6,40 @@ import cardStyle from './Card.module.scss'
 
 
 
-export default function Card({ text }) {
+export default function Card({ text, isArchived, isEmployee, isAdmin }) {
     const pathname = useLocation().pathname
 
     return (
-        <div className={cardStyle.cardContainer}>
+        <div className={`${cardStyle.cardContainer} ${isArchived ? cardStyle.archived : ''} `}>
             <h2 className={cardStyle.title}>{text}</h2>
             <div className={cardStyle.cardBox}>
                 <p className={cardStyle.subtitle}>0 курсов</p>
-                <p className={cardStyle.subtitle}>0 сотрудников</p>
+                {isAdmin ?<p className={cardStyle.subtitle}>0 сотрудников</p> : ''}
             </div>
-            {pathname === '/employee' ? (<div className={cardStyle.progressBar}>
+            {isEmployee ? (<div className={cardStyle.progressBar}>
                 <p className={cardStyle.progressBarTitle}>Не начат</p>
                 <div className={cardStyle.progressBarImage} />
-            </div>) : 
-             pathname === '/position' ? (<DropdownMenu />) :
-            (<RadioButton
-                type="checkbox"
-                name="checkbox"
-                className={cardStyle.cardCheckbox}
-            />)} 
+            </div>) : '' }
+                {pathname === '/position' ? (<DropdownMenu className={cardStyle.cardDropDownMenu} />) :
+                    (<RadioButton
+                        type='checkbox'
+                        name='checkbox'
+                        className={cardStyle.cardCheckbox}
+                    />)}
+            {isArchived ? (
+                <button type='button' className={cardStyle.buttonArchived}></button>
+            ) : ''}
         </div>
     )
 }
 
 Card.propTypes = {
-    text: string.isRequired
+    text: string.isRequired,
+    isArchived: bool,
+};
+
+Card.defaultProps = {
+    isArchived: false,
+    isEmployee: false,
+    isAdmin: false
 };
---
+++ src/components/ui-kit/Card/Card.stories.js
@@ -0,0 +1,11 @@
+import Card from './Card';
+
+export default {
+  title: 'ui-kit/Card',
+  component: Card,
+  tags: ['autodocs'],
+};
+
+export const Primary = {
+  args: {},
+};
