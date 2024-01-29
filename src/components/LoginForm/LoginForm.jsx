1a675089919cf45571fb98137eaeb2cb51b96d72
Kirill Karpov <rin.veber@gmail.com>
2024-01-29 21:50:41 +0400

    fix navigate

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -28,12 +28,14 @@ export default function LoginForm() {
     e.preventDefault();
     const values = {
       email: email.value,
-      password: password.value
-    }
-    dispatch(fetchToken(values))
-      if (localStorage.getItem('token' !== null)) {
-          navigate('/profile')
-      }
+      password: password.value,
+    };
+    dispatch(fetchToken(values));
+    // const token = localStorage.getItem('token');
+    navigate('/profile');
+    // if (token !== null) {
+    //   navigate('/profile');
+    // }
   };
   return (
     <div className={loginFormStyles.formContainer}>
